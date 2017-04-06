0549be919432c70b78c7204711fd920401d94921
Morten Laursen <ms.fuge@gmail.com>
2017-04-06 15:26:35 +0200

    logic for two and four dimentions added

---
+++ src/com/schultzz/fourDimension/Main.java
@@ -0,0 +1,10 @@
+package com.schultzz.fourDimension;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PSOLogic logic = new PSOLogic();
+        logic.runner();
+
+    }
+}
---
+++ src/com/schultzz/twoDimension/Main.java
@@ -0,0 +1,9 @@
+package com.schultzz.twoDimension;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PSOLogic logic = new PSOLogic();
+        logic.runner();
+    }
+}
